1f950ce58cd0fe1ef162b43a2df975103ab84ba2
Theodore Calmes <theo@thoughtbot.com>
2013-07-10 22:57:01 -0700

    Initial Commit

---
+++ TBMath/TBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TBAppDelegate.h
+//  TBMath
+//
+//  Created by Theodore Calmes on 7/10/13.
+//  Copyright (c) 2013 Theodore Calmes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TBMathTests/TBMathTests.m
@@ -0,0 +1,36 @@
+//
+//  TBMathTests.m
+//  TBMathTests
+//
+//  Created by Theodore Calmes on 7/10/13.
+//  Copyright (c) 2013 Theodore Calmes. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TBMathTests : XCTestCase
+
+@end
+
+@implementation TBMathTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
