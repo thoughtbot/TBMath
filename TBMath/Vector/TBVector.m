04a596dacb5e2fae502e0762b0d2a78ffbc37fe6
Theodore Calmes <theo@thoughtbot.com>
2013-09-24 09:03:52 -0700

    Add Vector constructor

--- TBMath/Vector/TBVector.m
+++ TBMath/Vector/TBVector.m
@@ -7,3 +7,23 @@
 //
 
 #import "TBVector.h"
+#import <Accelerate/Accelerate.h>
+
+TBVector TBVectorMakeWithLength(int length, TBNumberType type, bool autoRelease)
+{
+    TBVector vector;
+    vector.type = type;
+    vector.length = length;
+    vector.autoRelease = autoRelease;
+
+    switch (type) {
+        case TBNumberTypeDouble:
+            vector.array.doubleArray = (double *)calloc(length, sizeof(double));
+            break;
+        case TBNumberTypeFloat:
+            vector.array.floatArray = (float *)calloc(length, sizeof(float));
+            break;
+    }
+
+    return vector;
+}
